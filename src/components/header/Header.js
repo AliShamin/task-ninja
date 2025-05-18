e16ee4761465570fa1ab9d0ca36ad7f8d5be639b
adnan07ali <155066795+adnan07ali@users.noreply.github.com>
2025-05-18 20:09:10 +0530

    fix: about button styles

--- src/components/header/Header.js
+++ src/components/header/Header.js
@@ -10,16 +10,22 @@ import { useRef } from "react";
 import DeleteCardsButton from "../deleteCards/DeleteCardsButton";
 import ExportCards from "../exportCards/ExportCards";
 import ImportCards from "../importCards/ImportCards";
+import { useNavigate } from 'react-router-dom';
 
 function Header() {
     const themeRef = useRef();
     useThemeService(themeRef, 'dark-box');
+    const navigate = useNavigate();
+    const handleClick = () => {
+    navigate('/'); // or navigate('https://external.com') for full URLs
+  };
+
     return (
         <>
             <header ref={themeRef} className="header-container container sticky align-center ">
                 <div className="w80 d-flex-row align-center space-btw">
-                    <div className="align-center space-btw">
-                        <div className="d-flex-row font-style-1 plr2">
+                    <div className="align-center space-btw cursor-pointer">
+                        <div className="d-flex-row font-style-1 plr2" onClick={handleClick}>
                             <img id="header_img" src={logo} />
                             <span id="header-title">Task Ninja</span>
                         </div>
--- src/components/infoButton/InfoButton.js
+++ src/components/infoButton/InfoButton.js
@@ -1,12 +1,19 @@
-import { FaBars } from 'react-icons/fa';import { Tooltip } from 'react-tooltip'
-import { Link } from 'react-router-dom';
+import { CgInfo } from "react-icons/cg";
+import { Tooltip } from 'react-tooltip'
+import { FaBars } from 'react-icons/fa';
+import { useNavigate } from 'react-router-dom';
 
 function InfoButton() {
+    const navigate = useNavigate();
+    const handleClick = () => {
+    navigate('/about'); // or navigate('https://external.com') for full URLs
+  };
     return (
         <>
-         <Link to="/about">
-            <FaBars size='20' className="mlr8px cursor-pointer" data-tooltip-id="my-info" data-tooltip-content="Info"/>
-         </Link> {/* Link to About Page */}
+            <span  className="plr1 cursor-pointer" data-tooltip-id="my-info" data-tooltip-content="Info"  onClick={handleClick}>
+                <FaBars size='18' color="#333" />
+            </span>
+            {/* <FaBars size='20' color="black" className="mlr8px cursor-pointer" data-tooltip-id="my-info" data-tooltip-content="Info"/> */}
             <Tooltip id="my-info"/>
          </>
     )
